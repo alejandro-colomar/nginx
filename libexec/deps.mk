078f124b9b73171a58dd106ee9191081c4da0738
Alejandro Colomar <alx.manpages@gmail.com>
2021-05-09 20:13:01 +0200

    deps.mk: srcfix
    Signed-off-by: Alejandro Colomar <alx.manpages@gmail.com>

--- libexec/deps.mk
+++ libexec/deps.mk
@@ -16,7 +16,7 @@ deps-build: basic-deps
 	chmod 755 . && \
 	equivs-build $(ROOTDIR)/etc/debian/build-deps 2>/dev/null \
 	| grep '^dpkg-deb' \
-	| grep -o "\./.*\.deb" \
+	| grep -o '\./.*\.deb' \
 	| xargs sudo apt-get install --no-install-recommends -y -q=2;
 
 .PHONY: deps-run
@@ -25,7 +25,7 @@ deps-run: submodules basic-deps
 	chmod 755 . && \
 	equivs-build $(ROOTDIR)/etc/debian/run-deps 2>/dev/null \
 	| grep '^dpkg-deb' \
-	| grep -o "\./.*\.deb" \
+	| grep -o '\./.*\.deb' \
 	| xargs sudo apt-get install --no-install-recommends -y -q=2;
 
 .PHONY: submodules
